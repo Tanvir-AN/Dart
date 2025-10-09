62e45622953b5e5ad9fbc6d94b8bb9f95b680db4
Tanvir Ahmed <tanvirahmed199823@gmail.com>
2025-10-10 00:45:40 +0600

    add data type value

--- Ostad 07/Lice 2/data type.dart
+++ Ostad 07/Lice 2/data type.dart
@@ -15,13 +15,18 @@ void main(){
   print('Single cote');
   print('When use can\'t');  //! Here It Called ( \ ) Escape Sequence or Backward Slash , it use for to continu the Execution
   print("Single cote");
+  //! '''Multi line String''' Here it can show exactly what we write in the print
   print('''Single 
   kslkhfshfsaf          
   cote
   ''');
+  // insite the Print Integer Value ( int )
   print(23263);
+  // insite the Print Floating Value ( double )
   print(52.546);
+  // insite the Print Boolean Value ( bool )
+  print(true);
 
-  print('hi');
-
-}
+  //for check the data type of any value we use ( runtimeType )
+  print('Ostad'.runtimeType);  // String
+} 
