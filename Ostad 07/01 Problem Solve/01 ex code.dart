21cb275433f6b742d64e8757d3d9e260cbe15956
Tanvir Ahmed <tanvirahmed199823@gmail.com>
2025-10-29 20:01:37 +0600

    oop concept and add ex code and problem solve

---
+++ Ostad 07/01 Problem Solve/01 ex code.dart
@@ -0,0 +1,15 @@
+void main (){
+ studentClasslassName('one', 2);
+ print(studentClasslassName('6', 1));
+
+ Map<String, String> name  = {
+  'a ': 'b',
+  'b ': 'c'
+ };
+ print(name['c']);
+
+}
+
+studentClasslassName (String clssName, int sectionNumber){
+  print('class $clssName section $sectionNumber');
+}
---
+++ Ostad 07/Modul 03/01_Theoretical_concept OOP.dart
@@ -0,0 +1,36 @@
+// OOP -> Object Oriented Programming
+// DOD -> Department of Defense -> America company -> 1st resecarch on OOP in 1960
+// OOP is a programming paradigm that uses objects and their interactions to model real-world entities and their relationships.
+
+// good software have 4 behavior
+// Software must be work -> it's slow but work -> not crash, not error, etc
+// it can extandable -> new feature can be added -> without change the old code and without crashing the code/software
+// it can be reusable -> it can be used multiple times -> without change the old code and without crashing the code/software
+// folt tolerant -> it can tolerate any kind of problem -> without crashing the code/software software run -> like without network connection FB show cash and notify user connect network but software work
+
+// OOP have 4 main goals or concepts
+// 1. Correctness -> software must be work correctly and not crash, not error, etc
+// 2. Robustness -> reactive agent action must be perform correctly -> without performing wrong action
+// 3. Extendibility -> system must be extandable -> can be add new feature and functionality without any problem -> adding new feature must be faster
+// 4. Reusability -> code must be reusable -> can be used multiple times without change the old code and without crashing the code
+
+// There are 4 main pillars in OOP
+// 1. Abstraction -> hide the implementation details and complexity code from the user 
+// 2. Polymorphism -> allow different objects to be treated as the same type -> woek same way but have different face
+// 3. Inheritance -> allow a class to inherit properties and methods from another class -> child class inherit properties wealth
+// 4. Encapsulation -> the process of hiding the implementation details and complexity code from the user
+
+// any programming have 4 main pillars of oop it's called object oriented programming language
+
+// Class , Object , extends , implements , scope(private , public) , Abstract class , interface
+
+// problem can solve by a patterns ->  (Design Pattern) -> each Design pattern can solve specific problem
+// after combine Design pattern we create a architecture
+
+//! learn SOLID Principles -> Single Responsibility , Open-Closed , Liskov Substitution , Interface Segregation , Dependency Inversion
+
+//! Book -> The Object-Oriented Thought Process - Matt Weisfeld -> to learn OOP concept clearly
+//! Blog -> Martin Fowler -> Martin Fowler researcher -> read his blog and book
+//! Blog -> Kent Beck -> Kent Beck researcher -> read his blog and book
+//! Blog -> Robert C. Martin -> Robert C. Martin researcher -> read his blog and book
+//! website -> https://blog.cleancoder.com -> write clean code and clearly understand code
